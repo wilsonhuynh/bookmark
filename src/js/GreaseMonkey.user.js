2da3f67d0b6ba49269d9ccb5e2cde0f259642c38
Wilson Huynh <wilson.huynh@webjet.com.au>
2018-05-13 23:45:13 +1000

    Updated userscript file.

--- src/js/GreaseMonkey.user.js
+++ src/js/GreaseMonkey.user.js
@@ -11,54 +11,17 @@
 // ==/UserScript==
 
 (function() {
-	if (window.top != window.self)  //don't run on frames or iframes
-    return;
 
-	// function to add a javascript file to page
-	function addScript(url) {
-		var element = document.createElement('script');
-		element.type ="text/javascript";
-		element.src = url;
-
-		addFile(element);
+	function addTools() {
+		var s = document.createElement('script');
+		s.src = 'https://cdn.rawgit.com/wilsonhuynh/bookmark/bf603285/src/js/tools.js';
+		document.head.appendChild(s);
 	}
 
-	// function to add a css file to page
-	function addStyle(url) {
-		var element = document.createElement('link');
-		element.type = "text/css";
-		element.rel = "stylesheet";
-		element.href = url;
+	//don't run on frames or iframes
+	if (window.top === window.self) 
+		document.addEventListener("DOMContentLoaded", addTools);
 
-		addFile(element);
-	}
-
-	function addFile(element) {
-		var title = document.getElementsByTagName('head')[0];
-		document.head.appendChild(element);
-	}
-
-
-	var jQueryRequired = 1;
-	function addUIResources () {
-		//check if jQuery is loaded
-		if (typeof jQuery=='undefined') {
-			if (jQueryRequired == 1) {
-				jqueryRequired = 100;
-				addScript('//code.jquery.com/jquery-1.12.4.min.js');
-			}
-
-			setTimeout(addUIResources, 1000);
-		}
-		else{
-			addStyle('https://cdn.rawgit.com/wilsonhuynh/bookmark/cb7d9a03/src/css/common.css');
-			addScript('//cdnjs.cloudflare.com/ajax/libs/Base64/0.3.0/base64.js');
-			addScript('https://cdn.rawgit.com/wilsonhuynh/bookmark/cb7d9a03/src/js/Common.js?v=0.1');
-		}
-	}
-
-	// add page resources when page ready
-	document.addEventListener("DOMContentLoaded", addUIResources);
 })();
 
 
